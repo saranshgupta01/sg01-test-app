e34fbcae23fd86f87719d932865f327e977979f7
Saransh Gupta <37132366+saranshgupta01@users.noreply.github.com>
2021-05-12 22:09:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
-var gplay = require('google-play-scraper');
+
 ////////////////////////////////////////////
 let app = express();
 var port = process.env.PORT || 3002;
@@ -25,46 +25,12 @@ app.get('/', function (req, res, next) {
 });
 ///////////////////////////////////////////
 
-app.post('/getAppInfo', async function (req, res, next) {
+app.post('/setDriverLocation', async function (req, res, next) {
 	var params = req.body;
-	const { appIds } = params;
-	getPackageDataHandler(appIds).then(appData => {
-		res.jsonp({ "status": 1, data: appData });
-	}).catch(err => {
-		res.jsonp({ "status": 0, data: null });
-	});
-
+	console.log("Get Driver Location----",params )
 });
 
-const getPackageDataHandler = async (idsData) => {
-	let appsData = [];
-	for (let i = 0; i < idsData.length; i++) {
-		let dt = await gplay.app({ appId: idsData[i] })
-			.then((appdata) => {
-				const { title = "", genreId = "", genre = "", appId = "", url = "" } = appdata;
-				let collectData = { title, genreId, genre, appId, url };
-				return collectData;
-			}).catch(err => console.log("eeee", err))
-		appsData.push(dt)
-	}
-	return appsData;
-}
 
-app.get('/getAppInfo', function (req, res, next) {
-	let appId = req.query.appId;
-	if (!appId) { res.jsonp({ "status": 0, "message": 'send app id in request' }); }
-	gplay.app({ appId: appId })
-		.then(appdata => {
-			const { title = "", genreId = "", genre = "", appId = "", url = "" } = appdata;
-			let collectData = { title, genreId, genre, appId, url };
-			res.jsonp({ "status": 1, "message": 'success', data: collectData });
-		}).catch(err => {
-			console.log(err)
-			res.jsonp({ "status": 0, "message": 'error', data: null });
-		});
-});
-
-app.get('/getAppCat', function (req, res, next) {
-	let appCat = gplay.category;
-	res.jsonp({ "status": 1, "message": 'success', data: appCat });
+app.get('/getTest', function (req, res, next) {
+	console.log("Test Working ")
 });
